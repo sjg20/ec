2ee36ca1fe8abe302a1d1da9ff0fa4501ebbb44b
Paul Fagerburg <pfagerburg@google.com>
2020-10-21 14:36:39 -0600

    chameleon: implement system monitor
    Change sysmon_init to start a separate task that reads the ADC inputs
    and saves the readings internally.
    Remove the `read_adcs` command and replace with `sysmon` to print the
    values in a friendly format.
    
    BUG=b:169365982
    TEST=build and program to the board. In the shell run `sysmon` and
    check that the reported values are reasonable. Note that the SOM
    values won't be reasonable unless/until the FPGA SOM is installed
    and powered up.
    
    Change-Id: I72a4912c60f2fa9d25a0154021abc39dd130cbcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/zephyr-chrome/+/2490656
    Tested-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Yuval Peress <peress@chromium.org>
    Reviewed-by: Jett Rink <jettrink@chromium.org>
    Commit-Queue: Paul Fagerburg <pfagerburg@chromium.org>

--- zephyr/projects/chameleon/include/sysmon.h
+++ zephyr/projects/chameleon/include/sysmon.h
@@ -10,6 +10,40 @@
 
 #include <zephyr.h>
 
+/**
+ * @brief Define all of the monitored inputs.
+ */
+enum sysmon_input {
+	SYSMON_VMON_12V = 0,	/**< VMON_12V, scaled to 3V. */
+	SYSMON_CMON_SOM,	/**< CMON_PP12000_SOM, scaled to 1V/A. */
+	SYSMON_CMON_BOARD,	/**< CMON_PP12000_CHAMELEON, scaled to 1V/A. */
+
+	/* These are connected to SYSMON1..8 through the "NO" terminals of the
+	 * analog switches (SYSMON_SEL_REGULATORS).
+	 */
+	SYSMON_VMON_PP1200,	/**< PP1200, the 1.2V supply. */
+	SYSMON_VMON_PP1360,	/**< PP1360, the 1.36V supply. */
+	SYSMON_VMON_PP1260,	/**< PP1260, the 1.26V supply. */
+	SYSMON_VMON_PP1000,	/**< PP1000, the 1.00V supply. */
+	SYSMON_VMON_9V,		/**< VMON_9V, scaled to 2.25V. */
+	SYSMON_VMON_5V,		/**< VMON_5V, scaled to 1.25V. */
+	SYSMON_VMON_3V3,	/**< VMON_3V3, scaled to 2.2V. */
+	SYSMON_VMON_PP1800,	/**< PP1800, the 1.8V supply. */
+
+	/* These are connected to SYSMON1..6 through the "NC" terminals of the
+	 * analog switches (SYSMON_SEL_SOM). The "NC" terminals on the analog
+	 * switches for SYSMON7 and SYSMON8 are no-connect.
+	 */
+	SYSMON_SOM_VMON,	/**< VMON on the SOM. */
+	SYSMON_SOM_VMON_1V2,	/**< VMON_1V2 on the SOM. */
+	SYSMON_SOM_VMON_MGT,	/**< VMON_MGT on the SOM. */
+	SYSMON_SOM_VMON_1V8A,	/**< VMON_1V8A on the SOM. */
+	SYSMON_SOM_VMON_C8,	/**< "VMON_C8" on the SOM. */
+	SYSMON_SOM_VREF_CS,	/**< VREF_CS (CS=current sense), 0V. */
+
+	SYSMON_VMON_NUM_INPUTS	/**< Number of monitored inputs. */
+};
+
 /** @brief Initialize the system monitor
  *
  * sysmon is responsible for reading several ADC channels to monitor
@@ -23,5 +57,17 @@
  * @retval -ENODEV or error return from gpio_pin_configure if error.
  */
 int sysmon_init(void);
+
+/**
+ * @brief Get the value of any of the monitored inputs
+ *
+ * This function will return the interpreted value of any of the monitored
+ * inputs, so anything that is scaled into the ADC will be scaled back up
+ * to the range of the actual signal.
+ *
+ * @param input Which of the monitored inputs to return the value.
+ * @retval The value of the monitored input.
+ */
+float sysmon_get_val(enum sysmon_input input);
 
 #endif
--- zephyr/projects/chameleon/src/sysmon.c
+++ zephyr/projects/chameleon/src/sysmon.c
@@ -14,12 +14,15 @@
 #include <sys/printk.h>
 #include "sysmon.h"
 
-#define SEL_GPIO	DT_GPIO_LABEL(DT_PATH(zephyr_user), sysmon_sel_gpios)
-#define SEL_PIN		DT_GPIO_PIN(DT_PATH(zephyr_user), sysmon_sel_gpios)
+#define SEL_GPIO DT_GPIO_LABEL(DT_PATH(zephyr_user), sysmon_sel_gpios)
+#define SEL_PIN DT_GPIO_PIN(DT_PATH(zephyr_user), sysmon_sel_gpios)
 
+/**
+ * @brief Input selection for the analog switches.
+ */
 enum sysmon_sel {
-	SYSMON_SEL_SOM,		/**< Select the FPGA modules voltage rails. */
-	SYSMON_SEL_REGULATORS	/**< Select the regulator voltage rails. */
+	SYSMON_SEL_SOM, /**< Select the FPGA modules voltage rails. */
+	SYSMON_SEL_REGULATORS /**< Select the regulator voltage rails. */
 };
 
 struct adc_hdl {
@@ -31,7 +34,8 @@ struct adc_hdl {
 /** @brief The STM32 ADC resolution is fixed at 12 bits. */
 #define STM_ADC_RESOLUTION 12
 
-/** @brief Declare the ADCs this module uses and the configuration.
+/**
+ * @brief Declare the ADCs this module uses and the configuration.
  *
  * Note that this module does not use ADC2.
  * 1. All of the analog signals are connected to ADC3 inputs, or inputs that
@@ -63,6 +67,154 @@ static struct adc_hdl adc_list[] = {
 
 };
 
+/**
+ * @brief Storage for the ADC readings for each monitored voltage.
+ */
+static uint16_t adc_reading[SYSMON_VMON_NUM_INPUTS];
+
+/**
+ * @brief Mutex protecting access to adc_reading.
+ */
+static K_MUTEX_DEFINE(adc_reading_mutex);
+
+/**
+ * @brief Define a struct with details for each of the monitored voltages.
+ *
+ * This struct is indexed by the enum for the monitored voltage. It
+ * provides the setting for the analog switches, which ADC to read (of
+ * the ADCs in adc_list[]), the channel to read, a scaling factor when
+ * converting from ADC reading to floating-point, and a name for display
+ * purposes.
+ * The scale_factor is 1.0 for most of the voltages, but some of them are
+ * scaled in hardware to a 3.3V input range. For those voltages, the
+ * scale_factor is written as `full_range/scaled_range` and we let the
+ * do constant folding.
+ */
+struct sysmon_adc_input {
+	enum sysmon_sel sel; /**< Which way to flip the analog switches. */
+	int adc_num; /**< Index into adc_list[] for this signal. */
+	int channel; /**< ADC channel to read. */
+	float scale_factor; /**< Scaling factor. */
+	const char *name; /**< Human-readable name of the signal. */
+};
+
+/**
+ * @brief Map each monitored voltage to the correct `sysmon_adc_input` values.
+ */
+static struct sysmon_adc_input scan_table[SYSMON_VMON_NUM_INPUTS] = {
+	/* Note that SYSMON_VMON_12V and the CMON inputs are not multiplexed,
+	 * so the `sel` value doesn't actually matter.
+	 */
+	[SYSMON_VMON_12V] = { .sel = SYSMON_SEL_REGULATORS,
+			      .adc_num = 0,
+			      .channel = 10,
+			      .scale_factor = 12.0 / 3.0,
+			      .name = "12V" },
+	[SYSMON_CMON_SOM] = { .sel = SYSMON_SEL_REGULATORS,
+			      .adc_num = 0,
+			      .channel = 11,
+			      .scale_factor = 1.0,
+			      .name = "CMON_SOM" },
+	[SYSMON_CMON_BOARD] = { .sel = SYSMON_SEL_REGULATORS,
+				.adc_num = 0,
+				.channel = 12,
+				.scale_factor = 1.0,
+				"CMON_BOARD" },
+
+	[SYSMON_VMON_PP1200] = { .sel = SYSMON_SEL_REGULATORS,
+				 .adc_num = 0,
+				 .channel = 13,
+				 .scale_factor = 1.0,
+				 .name = "PP1200" },
+	[SYSMON_VMON_PP1360] = { .sel = SYSMON_SEL_REGULATORS,
+				 .adc_num = 0,
+				 .channel = 14,
+				 .scale_factor = 1.0,
+				 .name = "PP1360" },
+	[SYSMON_VMON_PP1260] = { .sel = SYSMON_SEL_REGULATORS,
+				 .adc_num = 0,
+				 .channel = 15,
+				 .scale_factor = 1.0,
+				 .name = "PP1000" },
+	[SYSMON_VMON_PP1000] = { .sel = SYSMON_SEL_REGULATORS,
+				 .adc_num = 1,
+				 .channel = 4,
+				 .scale_factor = 1.0,
+				 .name = "PP1260" },
+	[SYSMON_VMON_9V] = { .sel = SYSMON_SEL_REGULATORS,
+			     .adc_num = 1,
+			     .channel = 5,
+			     .scale_factor = 9.0 / 2.25,
+			     .name = "VMON_9V" },
+	[SYSMON_VMON_5V] = { .sel = SYSMON_SEL_REGULATORS,
+			     .adc_num = 1,
+			     .channel = 6,
+			     .scale_factor = 5.0 / 1.25,
+			     .name = "VMON_5V" },
+	[SYSMON_VMON_3V3] = { .sel = SYSMON_SEL_REGULATORS,
+			      .adc_num = 1,
+			      .channel = 7,
+			      .scale_factor = 3.3 / 2.2,
+			      .name = "VMON_3V3" },
+	[SYSMON_VMON_PP1800] = { .sel = SYSMON_SEL_REGULATORS,
+				 .adc_num = 1,
+				 .channel = 8,
+				 .scale_factor = 1.0,
+				 .name = "PP1800" },
+
+	[SYSMON_SOM_VMON] = { .sel = SYSMON_SEL_SOM,
+			      .adc_num = 0,
+			      .channel = 13,
+			      .scale_factor = 1.0,
+			      .name = "SOM_VMON" },
+	[SYSMON_SOM_VMON_1V2] = { .sel = SYSMON_SEL_SOM,
+				  .adc_num = 0,
+				  .channel = 14,
+				  .scale_factor = 1.0,
+				  .name = "SOM_VMON_1V2" },
+	[SYSMON_SOM_VMON_MGT] = { .sel = SYSMON_SEL_SOM,
+				  .adc_num = 0,
+				  .channel = 15,
+				  .scale_factor = 1.0,
+				  .name = "SOM_VMON_MGT" },
+	[SYSMON_SOM_VMON_1V8A] = { .sel = SYSMON_SEL_SOM,
+				   .adc_num = 1,
+				   .channel = 4,
+				   .scale_factor = 1.0,
+				   .name = "SOM_VMON_1V8A" },
+	[SYSMON_SOM_VMON_C8] = { .sel = SYSMON_SEL_SOM,
+				 .adc_num = 1,
+				 .channel = 5,
+				 .scale_factor = 1.0,
+				 .name = "SOM_VMON_C8" },
+	[SYSMON_SOM_VREF_CS] = { .sel = SYSMON_SEL_SOM,
+				 .adc_num = 1,
+				 .channel = 6,
+				 .scale_factor = 1.0,
+				 .name = "SOM_VREF_CS" },
+
+};
+
+/**
+ * Define priotity for the sysmon task
+ */
+#define SYSMON_PRIORITY 5
+
+/**
+ * Define the size of the stack for the sysmon task.
+ */
+#define SYSMON_STACK_SIZE 500
+
+/**
+ * Zephyr macro to allocate the stack area.
+ */
+K_THREAD_STACK_DEFINE(sysmon_stack_area, SYSMON_STACK_SIZE);
+
+/**
+ * Internal data structure for tracking the sysmon task.
+ */
+static struct k_thread sysmon_thread_data;
+
 /** @brief Select which set of voltages SYSMON can read
  *
  * There are more voltages to monitor than ADC inputs, so the hardware
@@ -71,6 +223,14 @@ static struct adc_hdl adc_list[] = {
  * (or voltage divider) outputs to the SYSMON inputs. When SYSMON_SEL
  * is low, the analog switches connect voltage outputs from the FPGA
  * module to the SYSMON inputs.
+ *
+ * The TS3A5018 analog switches have a max turn-on time of 9ns, and a max
+ * turn-off time of 7ns (table 6.9, 3.3V supply, over full operating free-air
+ * temp range). The STM32 is running at 72 MHz, which is a 14ns clock.
+ * The switching time for the analog switches is less than a single
+ * machine instruction. Therefore we don't need to have any extra delays in
+ * the code between setting the analog switches and then using the signals
+ * that are routed through the switches.
  *
  * @param sel SYSMON_SEL_REGULATORS to select the voltage regulator/divider,
  * or SYSMON_SEL_SOM to select the FPGA SOM voltages.
@@ -81,8 +241,8 @@ static struct adc_hdl adc_list[] = {
 static int sysmon_sel(enum sysmon_sel sel)
 {
 	int ret;
-	int gpio_dir = (sel == SYSMON_SEL_REGULATORS) ?
-		GPIO_OUTPUT_HIGH : GPIO_OUTPUT_LOW;
+	int gpio_dir = (sel == SYSMON_SEL_REGULATORS) ? GPIO_OUTPUT_HIGH :
+							      GPIO_OUTPUT_LOW;
 	const struct device *gpio_dev = device_get_binding(SEL_GPIO);
 
 	if (gpio_dev == NULL) {
@@ -99,8 +259,128 @@ static int sysmon_sel(enum sysmon_sel sel)
 	return 0;
 }
 
+/**
+ * @brief Store the ADC value for a particular input
+ *
+ * This internal function provides a way for sysmon to store the ADC readings
+ * without worrying about how the tables are organized.
+ *
+ * @param input Which of the monitored inputs is being set.
+ * @param reading The ADC reading to store.
+ */
+static void sysmon_set_val(enum sysmon_input input, uint16_t reading)
+{
+	__ASSERT(input < SYSMON_VMON_NUM_INPUTS && (int)input >= 0,
+		 "Invalid enum passed to sysmon_set");
+
+	k_mutex_lock(&adc_reading_mutex, K_FOREVER);
+	adc_reading[input] = reading;
+	k_mutex_unlock(&adc_reading_mutex);
+}
+
+/**
+ * @brief Read the ADCs sequentially in a loop forever.
+ *
+ * The sysmon task will step through the list of inputs in an infinite loop.
+ * For each input, it will set the analog switches to the correct setting,
+ * read the channel from the ADC, and store the reading in the adc_reading
+ * array. When it reaches the end of the list, it starts over at the beginning.
+ * Between each reading, the thread sleeps to give other threads a chance
+ * to run.
+ *
+ * @param unused1 Required by the Zephyr thread API. Unused.
+ * @param unused2 Required by the Zephyr thread API. Unused.
+ * @param unused3 Required by the Zephyr thread API. Unused.
+ */
+static void sysmon_task(void *unused1, void *unused2, void *unused3)
+{
+	ARG_UNUSED(unused1);
+	ARG_UNUSED(unused2);
+	ARG_UNUSED(unused3);
+
+	uint16_t sample_buffer;
+	int idx = 0;
+
+	k_thread_name_set(NULL, "sysmon_task");
+
+	/* Step through the scan_table one entry at a time, resetting to 0
+	 * when we get to the end. Loop forever.
+	 */
+	for (;;) {
+		/* Read the ADCs one channel at a time. The Zephyr driver
+		 * for the STM32 ADC doesn't seem to like a sequence of more
+		 * than one ADC channel, even though the hardware supports it.
+		 */
+		const struct adc_sequence seq = {
+			.channels = BIT(scan_table[idx].channel),
+			.buffer = &sample_buffer,
+			.buffer_size = sizeof(sample_buffer),
+			.resolution = STM_ADC_RESOLUTION,
+		};
+
+		if (sysmon_sel(scan_table[idx].sel) >= 0) {
+			if (adc_read(adc_list[scan_table[idx].adc_num].dev,
+				     &seq) >= 0) {
+				sysmon_set_val((enum sysmon_input)idx,
+					       sample_buffer);
+			}
+		}
+
+		idx++;
+		if (idx >= (int)SYSMON_VMON_NUM_INPUTS) {
+			idx = 0;
+		}
+
+		/*
+		 * Add a delay before moving to the next channel, so we
+		 * don't starve the system. 50ms was an ad-hoc choice, and
+		 * is subject to adjustment as more tasks are added to
+		 * the system.
+		 */
+		k_sleep(K_MSEC(50));
+	}
+}
+
+float sysmon_get_val(enum sysmon_input input)
+{
+	__ASSERT((int)input < (int)SYSMON_VMON_NUM_INPUTS && (int)input >= 0,
+		 "Invalid enum passed to sysmon_get");
+
+	/* Get the reading inside a mutex. */
+	k_mutex_lock(&adc_reading_mutex, K_FOREVER);
+	uint16_t reading = adc_reading[input];
+
+	k_mutex_unlock(&adc_reading_mutex);
+
+	/* Convert ADC reading to 3.3V. */
+	float val = (float)reading;
+
+	/* This division is by a power of two, so it will only adjust the
+	 * floating-point exponent; we won't lose precision in the mantissa.
+	 */
+	val /= (float)(1 << STM_ADC_RESOLUTION);
+	val *= 3.3;
+	val *= scan_table[input].scale_factor;
+
+	return val;
+}
+
 int sysmon_init(void)
 {
+	/*
+	 * Do not allow more than one call to sysmon_init.
+	 * The main problem with calling it more than once is that each
+	 * thread created here will wind up using the same area of memory
+	 * for a stack. They also will expect to have exclusive control
+	 * over the SYSMON_SEL signal, and if one thread changes it to
+	 * a value that the other thread doesn't think is active, the
+	 * wrong signals will be read into the adc_readinds[] array.
+	 */
+	static bool initted;
+
+	__ASSERT(!initted, "sysmon_init called more than once");
+	initted = true;
+
 	int ret = 0;
 
 	/* Set the analog switches for the regulator outputs. */
@@ -110,74 +390,48 @@ int sysmon_init(void)
 	}
 
 	/* Configure the ADCs. */
-	for (int idx = 0 ; idx < ARRAY_SIZE(adc_list) ; ++idx) {
-		adc_list[idx].dev = device_get_binding(
-			adc_list[idx].device_name);
+	for (int idx = 0; idx < ARRAY_SIZE(adc_list); ++idx) {
+		adc_list[idx].dev =
+			device_get_binding(adc_list[idx].device_name);
 		if (adc_list[idx].dev == NULL) {
 			printk("Cannot get device %s\n",
-				adc_list[idx].device_name);
+			       adc_list[idx].device_name);
 			return -ENODEV;
 		}
 
 		ret = adc_channel_setup(adc_list[idx].dev,
-			&adc_list[idx].channel_config);
+					&adc_list[idx].channel_config);
 		if (ret < 0) {
 			printk("adc_channel_setup failed, ret = %d\n", ret);
 			return ret;
 		}
 	}
 
+	/* Create the sysmon task and start it. */
+	k_thread_create(&sysmon_thread_data, sysmon_stack_area,
+			K_THREAD_STACK_SIZEOF(sysmon_stack_area), sysmon_task,
+			NULL, NULL, NULL, SYSMON_PRIORITY, 0, K_NO_WAIT);
+
 	return ret;
 }
 
-/* Temporary function that reads all of the ADCs and prints the raw values.
- * If this code looks bad to you, that's OK; it's supposed to look bad.
- * The code is only a quick-and-dirty proof that we can read the ADCs and
- * get back values that are sensible. It's not supposed to be pretty or
- * even maintainable. The next revision (done under b/169365982) will
- * remove this code entirely and replace it with actually clean code. But
- * for the purposes of checkpointing progress, this code exists.
+/**
+ * @brief Display all of the monitored values in a friendly format
+ *
+ * @param shell Our shell data struct; needed for some shell APIs.
+ * @param argc Count of arguments passed to this shell command. Unused.
+ * @param argv Arguments for this shell command. Unused.
  */
-static int cmd_read_adcs(const struct shell *shell, size_t argc, char **argv)
+static int cmd_sysmon(const struct shell *shell, size_t argc, char **argv)
 {
 	ARG_UNUSED(argc);
 	ARG_UNUSED(argv);
 
-	int ret;
-	uint16_t sample_buffer;
-
-	for (int chan = 10 ; chan <= 15 ; ++chan) {
-		const struct adc_sequence sequence = {
-			.channels	= BIT(chan),
-			.buffer		= &sample_buffer,
-			.buffer_size	= sizeof(sample_buffer),
-			.resolution	= STM_ADC_RESOLUTION,
-		};
-
-		ret = adc_read(adc_list[0].dev, &sequence);
-		if (ret < 0) {
-			printk("adc_read failed, ret = %d\n", ret);
-			return ret;
-		}
-		printk("%u\n", sample_buffer);
+	for (int idx = 0; idx < (int)SYSMON_VMON_NUM_INPUTS; ++idx) {
+		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "%s: %f\n",
+			      scan_table[idx].name,
+			      sysmon_get_val((enum sysmon_input)idx));
 	}
-
-	for (int chan = 4 ; chan <= 8 ; ++chan) {
-		const struct adc_sequence sequence = {
-			.channels	= BIT(chan),
-			.buffer		= &sample_buffer,
-			.buffer_size	= sizeof(sample_buffer),
-			.resolution	= STM_ADC_RESOLUTION,
-		};
-
-		ret = adc_read(adc_list[1].dev, &sequence);
-		if (ret < 0) {
-			printk("adc_read failed, ret = %d\n", ret);
-			return ret;
-		}
-		printk("%u\n", sample_buffer);
-	}
-
 	return 0;
 }
-SHELL_CMD_REGISTER(read_adcs, NULL, "read the ADCs and print", cmd_read_adcs);
+SHELL_CMD_REGISTER(sysmon, NULL, "show the SYSMON values", cmd_sysmon);
