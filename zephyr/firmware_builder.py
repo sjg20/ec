f64a00a166a6f8c9421ab9d165a3166d423ef24c
LaMont Jones <lamontjones@chromium.org>
2020-12-16 17:17:52 -0700

    firmware_builder: support --metrics
    BUG=b:175248887
    TEST=CQ
    
    Cq-Include-Trybots: chromeos/cq:firmware-zephyr-cq
    Change-Id: I517d7cda38fdf95ef3c9d1724a2b83c8bf82b6ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/zephyr-chrome/+/2595242
    Tested-by: LaMont Jones <lamontjones@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: LaMont Jones <lamontjones@chromium.org>

--- zephyr/firmware_builder.py
+++ zephyr/firmware_builder.py
@@ -5,18 +5,27 @@
 # found in the LICENSE file.
 """Build and test all of the Zephyr boards.
 
-This is the entry point for the custom firmware builder workflow rZephyripe.
+This is the entry point for the custom firmware builder workflow recipe.
 """
 
 import argparse
+import multiprocessing
 import os
 import shutil
 import subprocess
 import sys
 
+from google.protobuf import json_format
+
+from chromite.api.gen.chromite.api import firmware_pb2
 
 def build(opts):
     """Builds all Zephyr firmware targets"""
+    # TODO(b/169178847): Add appropriate metric information
+    metrics = firmware_pb2.FwBuildMetricList()
+    with open(opts.metrics, 'w') as f:
+        f.write(json_format.MessageToJson(metrics))
+
     temp_build_dir = os.path.join('/tmp', 'zbuild')
     targets = [
         'projects/experimental/volteer', 'projects/experimental/posix-ec'
@@ -35,6 +44,12 @@ def build(opts):
 
 
 def test(opts):
+    """Runs all of the unit tests for Zephyr firmware"""
+    # TODO(b/169178847): Add appropriate metric information
+    metrics = firmware_pb2.FwTestMetricList()
+    with open(opts.metrics, 'w') as f:
+        f.write(json_format.MessageToJson(metrics))
+
     return subprocess.run(['zmake', 'testall', '--fail-fast']).returncode
 
 
@@ -46,12 +61,25 @@ def main(args):
         print("Must select a valid sub command!")
         return -1
 
-    # Run selectd sub command function
+    # Run selected sub command function
     return opts.func(opts)
 
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '--cpus',
+        default=multiprocessing.cpu_count(),
+        help='The number of cores to use.',
+    )
+
+    parser.add_argument(
+        '--metrics',
+        dest='metrics',
+        required=True,
+        help='File to write the json-encoded MetricsList proto message.',
+    )
 
     # Would make this required=True, but not available until 3.7
     sub_cmds = parser.add_subparsers()
