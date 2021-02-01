jackadam1981/ChromeEc

zephyr: Add support for BOARD_VERSION
Add Kconfig options for this feature.

At present system_get_board_version() is not built for Zephyr. We could
split it out but it seems better to wait until we have the existing file
brought into the build in toto.

BUG=b:178124020
BRANCH=none
TEST=make BOARD=volteer -j30
boot zephyr for volteer and see that the options are defined

Change-Id: I100c8f3f65db437fbe654e0230e07f59c6e0c7b4
Signed-off-by: Simon Glass <sjg@chromium.org>

zephyr/shim/include/config_chip.h
@@ -591,4 +591,19 @@ enum battery_type {
 #define CONFIG_VSTORE_SLOT_COUNT	CONFIG_PLATFORM_EC_VSTORE_SLOT_COUNT
 #endif
 
+#undef CONFIG_BOARD_VERSION_CBI
+#ifndef CONFIG_PLATFORM_EC_BOARD_VERSION_CBI
+#define CONFIG_BOARD_VERSION_CBI
+#endif
+
+#undef CONFIG_BOARD_VERSION_GPIO
+#ifndef CONFIG_PLATFORM_EC_BOARD_VERSION_GPIO
+#define CONFIG_BOARD_VERSION_GPIO
+#endif
+
+#undef CONFIG_BOARD_VERSION_CUSTOM
+#ifndef CONFIG_PLATFORM_EC_BOARD_VERSION_CUSTOM
+#define CONFIG_BOARD_VERSION_CUSTOM
+#endif
+
 #endif  /* __CROS_EC_CONFIG_CHIP_H */

sjg20/ec

zephyr/shim/include/config_chip.h
@@ -850,4 +850,19 @@ enum battery_type {
 #define CONFIG_CMD_STACKOVERFLOW
 #endif
 
+#undef CONFIG_BOARD_VERSION_CBI
+#ifdef CONFIG_PLATFORM_EC_BOARD_VERSION_CBI
+#define CONFIG_BOARD_VERSION_CBI
+#endif
+
+#undef CONFIG_BOARD_VERSION_GPIO
+#ifdef CONFIG_PLATFORM_EC_BOARD_VERSION_GPIO
+#define CONFIG_BOARD_VERSION_GPIO
+#endif
+
+#undef CONFIG_BOARD_VERSION_CUSTOM
+#ifdef CONFIG_PLATFORM_EC_BOARD_VERSION_CUSTOM
+#define CONFIG_BOARD_VERSION_CUSTOM
+#endif
+
 #endif  /* __CROS_EC_CONFIG_CHIP_H */